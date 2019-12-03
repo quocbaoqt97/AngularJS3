ac7bd13b489dcc4cfd93722763d0583ac85f4c67
Alex <Alexnguyen@coursera.com>
2019-12-03 10:45:36 +0700

    webapp

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 (function(){
+  'use strict'
   angular.module('webapp',[])
   .controller('webbappcontroller',webbappcontroller);
-  
-
-
-
+webbappcontroller.$inject=['$scope','$filter'];
+  function webbappcontroller($scope,$filter){
+    $scope.amount_1 = 0;
+    $scope.Displaymoney = function(){
+      var amount_2 = 23100*($scope.amount_1);
+      $scope.amount_2 = $filter('currency')(amount_2,"VND",3);
+    }
+  }
 })();
